318b6a5eb3d168c036614ad8c62641aed4c285cf
Xing Zhao <zhaoxing8325@gmail.com>
2022-11-10 10:38:13 -0800

    Initial Commit

---
+++ PinToBottomDemo/PinToBottomDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinToBottomDemoApp.swift
+//  PinToBottomDemo
+//
+//  Created by XING ZHAO on 2022-11-10.
+//
+
+import SwiftUI
+
+@main
+struct PinToBottomDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

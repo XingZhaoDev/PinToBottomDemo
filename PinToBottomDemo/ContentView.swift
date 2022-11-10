364e9629824b33183d5735a00cc182c5ec7d809c
Xing Zhao <zhaoxing8325@gmail.com>
2022-11-10 15:24:08 -0800

    added code

--- PinToBottomDemo/ContentView.swift
+++ PinToBottomDemo/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+struct Message: Identifiable {
+    var text: String
+    var id: UUID = UUID()
+}
 
 struct ContentView: View {
+    @State private var messages: [Message] = []
+    @State private var newMessageText = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List(messages) { message in
+                Text(message.text)
+            }
+            .safeAreaInset(edge: .bottom) {
+                TextField("New Message", text: $newMessageText)
+                    .padding()
+                    .textFieldStyle(.roundedBorder)
+                    .background(.ultraThinMaterial)
+                    .onSubmit {
+                        if !newMessageText.isEmpty {
+                            messages.append(Message(text: newMessageText))
+                            newMessageText = ""
+                        }
+                    }
+            }
+            .listStyle(.plain)
+            .navigationTitle("My messages")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+        .onAppear {
+            for i in 1...21 {
+                messages.append(Message(text: "Message \(i)"))
+            }
+        }
     }
 }
 
